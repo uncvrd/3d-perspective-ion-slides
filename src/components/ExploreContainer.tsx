b2c74057b1a4df4776157d109fd146667b61f0e4
Jordan Lewallen <support@uncvrd.co>
2020-07-23 03:27:05 -0700

    init commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import './ExploreContainer.css';
+import slideOpts from './slide-opts';
+import { IonSlides, IonSlide, IonButton } from '@ionic/react';
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+    return (
+        <IonSlides options={slideOpts}>
+            <IonSlide>
+                <div
+                    style={{
+                        height: '100vh',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <IonButton>Button 1</IonButton>
+                    <IonButton>Button 2</IonButton>
+                </div>
+            </IonSlide>
+            <IonSlide>
+                <div
+                    style={{
+                        height: '100vh',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <IonButton>Button 3</IonButton>
+                    <IonButton>Button 4</IonButton>
+                </div>
+            </IonSlide>
+        </IonSlides>
+    );
 };
 
 export default ExploreContainer;
---
+++ src/components/slide-opts.js
@@ -0,0 +1,151 @@
+const slideOpts = {
+    grabCursor: true,
+    cubeEffect: {
+      shadow: false,
+      slideShadows: false,
+    },
+    on: {
+      beforeInit: function() {
+        const swiper = this;
+        swiper.classNames.push(`${swiper.params.containerModifierClass}cube`);
+        swiper.classNames.push(`${swiper.params.containerModifierClass}3d`);
+  
+        const overwriteParams = {
+          slidesPerView: 1,
+          slidesPerColumn: 1,
+          slidesPerGroup: 1,
+          watchSlidesProgress: true,
+          resistanceRatio: 0,
+          spaceBetween: 0,
+          centeredSlides: false,
+          virtualTranslate: true,
+          threshold: 10
+        };
+  
+        this.params = Object.assign(this.params, overwriteParams);
+        this.originalParams = Object.assign(this.originalParams, overwriteParams);
+      },
+      setTranslate: function() {
+        const swiper = this;
+        const {
+          $el, $wrapperEl, slides, width: swiperWidth, height: swiperHeight, rtlTranslate: rtl, size: swiperSize,
+        } = swiper;
+        const params = swiper.params.cubeEffect;
+        const isHorizontal = swiper.isHorizontal();
+        const isVirtual = swiper.virtual && swiper.params.virtual.enabled;
+        let wrapperRotate = 0;
+        let $cubeShadowEl;
+        if (params.shadow) {
+          if (isHorizontal) {
+            $cubeShadowEl = $wrapperEl.find('.swiper-cube-shadow');
+            if ($cubeShadowEl.length === 0) {
+              $cubeShadowEl = swiper.$('<div class="swiper-cube-shadow"></div>');
+              $wrapperEl.append($cubeShadowEl);
+            }
+            $cubeShadowEl.css({ height: `${swiperWidth}px` });
+          } else {
+            $cubeShadowEl = $el.find('.swiper-cube-shadow');
+            if ($cubeShadowEl.length === 0) {
+              $cubeShadowEl = swiper.$('<div class="swiper-cube-shadow"></div>');
+              $el.append($cubeShadowEl);
+            }
+          }
+        }
+  
+        for (let i = 0; i < slides.length; i += 1) {
+          const $slideEl = slides.eq(i);
+          let slideIndex = i;
+          if (isVirtual) {
+            slideIndex = parseInt($slideEl.attr('data-swiper-slide-index'), 10);
+          }
+          let slideAngle = slideIndex * 90;
+          let round = Math.floor(slideAngle / 360);
+          if (rtl) {
+            slideAngle = -slideAngle;
+            round = Math.floor(-slideAngle / 360);
+          }
+          const progress = Math.max(Math.min($slideEl[0].progress, 1), -1);
+          let tx = 0;
+          let ty = 0;
+          let tz = 0;
+          if (slideIndex % 4 === 0) {
+            tx = -round * 4 * swiperSize;
+            tz = 0;
+          } else if ((slideIndex - 1) % 4 === 0) {
+            tx = 0;
+            tz = -round * 4 * swiperSize;
+          } else if ((slideIndex - 2) % 4 === 0) {
+            tx = swiperSize + (round * 4 * swiperSize);
+            tz = swiperSize;
+          } else if ((slideIndex - 3) % 4 === 0) {
+            tx = -swiperSize;
+            tz = (3 * swiperSize) + (swiperSize * 4 * round);
+          }
+          if (rtl) {
+            tx = -tx;
+          }
+  
+           if (!isHorizontal) {
+            ty = tx;
+            tx = 0;
+          }
+           const transform$$1 = `rotateX(${isHorizontal ? 0 : -slideAngle}deg) rotateY(${isHorizontal ? slideAngle : 0}deg) translate3d(${tx}px, ${ty}px, ${tz}px)`;
+          if (progress <= 1 && progress > -1) {
+            wrapperRotate = (slideIndex * 90) + (progress * 90);
+            if (rtl) wrapperRotate = (-slideIndex * 90) - (progress * 90);
+          }
+          $slideEl.transform(transform$$1);
+        }
+        $wrapperEl.css({
+          '-webkit-transform-origin': `50% 50% -${swiperSize / 2}px`,
+          '-moz-transform-origin': `50% 50% -${swiperSize / 2}px`,
+          '-ms-transform-origin': `50% 50% -${swiperSize / 2}px`,
+          'transform-origin': `50% 50% -${swiperSize / 2}px`,
+        });
+  
+        const zFactor = (swiper.browser.isSafari || swiper.browser.isUiWebView) ? (-swiperSize / 2) : 0;
+        $wrapperEl
+          .transform(`translate3d(0px,0,${zFactor}px) rotateX(${swiper.isHorizontal() ? 0 : wrapperRotate}deg) rotateY(${swiper.isHorizontal() ? -wrapperRotate : 0}deg) scale(${getScale(wrapperRotate, [1, 0.95])})`)
+      },
+      setTransition: function(duration) {
+        const swiper = this;
+        const { $el, slides } = swiper;
+        slides
+          .transition(duration)
+          .find('.swiper-slide-shadow-top, .swiper-slide-shadow-right, .swiper-slide-shadow-bottom, .swiper-slide-shadow-left')
+          .transition(duration);
+        if (swiper.params.cubeEffect.shadow && !swiper.isHorizontal()) {
+          $el.find('.swiper-cube-shadow').transition(duration);
+        }
+      },
+    }
+  }
+
+  export default slideOpts;
+
+  function getScale(x, scaleRange) {
+    const diff = Math.abs(x) % 90;
+
+    if (!diff) {
+        return 1;
+    }
+
+    const convert = diff > 45 ? linearConversion([90, 45], [1, 0.5]) : linearConversion([45, 0], [0.5, 1]);
+
+    const eased = circ(convert(diff));
+    const scale = linearConversion([1, 0.5], scaleRange);
+    return scale(eased);
+}
+
+function linearConversion(a, b) {
+    var o = a[1] - a[0],
+        n = b[1] - b[0];
+
+    return function (x) {
+        return ((x - a[0]) * n) / o + b[0];
+    };
+}
+
+function circ(timeFraction) {
+    return 1 - Math.sin(Math.acos(timeFraction));
+}
